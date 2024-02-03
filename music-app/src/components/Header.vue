224dd8d86bd5c9895e1290af105e2a9d6999b94c
Murrel Venlo <57919084+murrelvenlo@users.noreply.github.com>
2024-02-03 21:59:41 +0100

    154. Redirecting after Logging Out

--- music-app/src/components/Header.vue
+++ music-app/src/components/Header.vue
@@ -28,7 +28,7 @@
               <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
             </li>
           </template>
         </ul>
@@ -50,6 +50,14 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
+    },
+    signOut() {
+      this.userStore.signOut()
+
+      // console.log(this.$router)
+      if (this.$route.name === 'manage') {
+        this.$router.push({ name: 'home' })
+      }
     }
   }
 }
