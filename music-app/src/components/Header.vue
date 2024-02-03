a89ae1533359773b9df2797bffc5eca7785dca42
Murrel Venlo <57919084+murrelvenlo@users.noreply.github.com>
2024-02-03 18:25:11 +0100

    148. Naming Routes.

--- music-app/src/components/Header.vue
+++ music-app/src/components/Header.vue
@@ -5,17 +5,18 @@
       <!-- App Name -->
       <router-link
         class="text-white font-bold uppercase text-2xl mr-4"
-        to="/"
+        :to="{ name: 'home' }"
         exact-active-class="no-active"
         >Music</router-link
       >
 
+      <!-- I had 'to="/pageName"', but now I change all of them to ':to="{ name: 'about' }"'. This is good for a better structure or SEO -->
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <router-link class="px-2 text-white" to="/about">About</router-link>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
           </li>
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
@@ -24,7 +25,7 @@
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
