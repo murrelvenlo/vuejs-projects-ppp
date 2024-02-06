5e635adefd3fbcb61eb9af13443d91fc537a19c3
Murrel Venlo <57919084+murrelvenlo@users.noreply.github.com>
2024-02-06 20:05:41 +0100

    165. Handling Errors and Successful Uploads.

--- music-app/src/components/Upload.vue
+++ music-app/src/components/Upload.vue
@@ -77,10 +77,24 @@ export default {
             text_class: ''
           }) - 1
 
-        task.on('state_changed', (snapshot) => {
-          const progess = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          this.uploads[uploadIndex].current_progress = progess
-        })
+        task.on(
+          'state_changed',
+          (snapshot) => {
+            const progess = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            this.uploads[uploadIndex].current_progress = progess
+          },
+          (error) => {
+            this.uploads[uploadIndex].variant = 'bg-red-400'
+            this.uploads[uploadIndex].icon = 'fas fa-times'
+            this.uploads[uploadIndex].text_class = 'text-red-400'
+            console.log(error)
+          },
+          () => {
+            this.uploads[uploadIndex].variant = 'bg-green-400'
+            this.uploads[uploadIndex].icon = 'fas fa-check'
+            this.uploads[uploadIndex].text_class = 'text-green-400'
+          }
+        )
       })
       console.log(files)
     }
