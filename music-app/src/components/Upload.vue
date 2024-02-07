a9e14b76d74220f5f4cfdbf1a8598ca7eeb54468
Murrel Venlo <57919084+murrelvenlo@users.noreply.github.com>
2024-02-08 00:53:53 +0100

    180. Updating the list of songs after an Upload.

--- music-app/src/components/Upload.vue
+++ music-app/src/components/Upload.vue
@@ -53,6 +53,7 @@ export default {
       uploads: []
     }
   },
+  props: ['addSong'],
   methods: {
     upload($event) {
       this.is_dragover = false
@@ -102,9 +103,11 @@ export default {
             }
 
             song.url = await task.snapshot.ref.getDownloadURL()
-
             // the add fu will instruct firebase to generate an id
-            await songsCollection.add(song)
+            const songRef = await songsCollection.add(song)
+            const songSnapshot = await songRef.get()
+
+            this.addSong(songSnapshot)
 
             this.uploads[uploadIndex].variant = 'bg-green-400'
             this.uploads[uploadIndex].icon = 'fas fa-check'
--- music-app/src/views/Manage.vue
+++ music-app/src/views/Manage.vue
@@ -2,7 +2,7 @@
   <!-- Main Content -->
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
-      <app-upload ref="upload" />
+      <app-upload ref="upload" :addSong="addSong" />
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
@@ -45,14 +45,7 @@ export default {
   async created() {
     const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get()
 
-    snapshot.forEach((document) => {
-      const song = {
-        ...document.data(),
-        docID: document.id
-      }
-
-      this.songs.push(song)
-    })
+    snapshot.forEach(this.addSong)
   },
   methods: {
     // pass the updated song to the manage component here with is fu
@@ -62,6 +55,14 @@ export default {
     },
     removeSong(i) {
       this.songs.splice(i, 1)
+    },
+    addSong(document) {
+      const song = {
+        ...document.data(),
+        docID: document.id
+      }
+
+      this.songs.push(song)
     }
   }
   // beforeRouteLeave(to, from, next) {
