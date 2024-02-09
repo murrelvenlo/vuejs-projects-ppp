6292b23f2271dcb0e4cf6acce754603b90509013
Murrel Venlo <57919084+murrelvenlo@users.noreply.github.com>
2024-02-09 14:00:32 +0100

    203. Creating links with Hash Fragments.

--- music-app/src/views/Song.vue
+++ music-app/src/views/Song.vue
@@ -12,7 +12,7 @@
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
-        <i class="fas fa-play"></i>
+        <i class="fas" :class="{ 'fa-play': !playing, 'fa-pause': playing }"></i>
       </button>
       <div class="z-50 text-left ml-8">
         <!-- Song Info -->
@@ -22,7 +22,7 @@
     </div>
   </section>
   <!-- Form -->
-  <section class="container mx-auto mt-6">
+  <section class="container mx-auto mt-6" id="comments">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
@@ -109,6 +109,7 @@ export default {
   },
   computed: {
     ...mapState(useUserStore, ['userLoggedIn']),
+    ...mapState(usePlayerStore, ['playing']),
     sortedComments() {
       return this.comments.slice().sort((a, b) => {
         if (this.sort === '1') {
@@ -135,7 +136,7 @@ export default {
     this.getComments()
   },
   methods: {
-    ...mapActions(usePlayerStore, ['newSong']),
+    ...mapActions(usePlayerStore, ['newSong', 'toggleAudio']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
